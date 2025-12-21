7f272a3959993759df4e6e97bffd80ebac7c5ac2
Cmd-8 <chap.8william@gmail.com>
2025-12-21 18:51:25 -0500

    Test code

--- firmware/sender/main/test.c
+++ firmware/sender/main/test.c
@@ -1,49 +1,76 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
-bool message_status;
+bool system = 1;
+bool message_status = 1;
 char message_sent[] = "The message was sent";
 char message_not[] = "The message was not sent";
 
 
-typedef enum 
+typedef enum
 {
     ide_status,
     ready_status,
+    connected_status
 
 
 }state_status;
 
+state_status current_state = ide_status;
 
 void messageFunction()
 {
     if (message_status == 1)
     {
         printf("%s\n", message_sent);
-    }
+        current_state = connected_status;
+     }
     else
     {
-        print("%s\n", message_not);    
+        printf("%s\n", message_not);
     }
 }
 
 int main()
 {
-    state_status current_state = ide_status;
 
-    switch (current_state)    
+while(true)
+{
+    switch (current_state)
     {
-        case (ide_status):
+        case ide_status:
         messageFunction();
-
         break;
 
-        case (ready_status):
-        
+        case connected_status:
+        printf("Checking system.....\n");
+        sleep(3);
+
+        if (system == 1 )
+        {
+                printf("System ready to go\n");
+                current_state = ready_status;
+                sleep(3);
+        }
+
+        else {
+                printf("Not ready\n");
+                sleep(3);
+        }
+        break;
+
+        case ready_status:
+        printf("Machine is ready\n");
+        sleep(3);
+
+
         break;
 
     }
+        usleep(50000); //tiskdelay()
+}
 
     return 0;
 }
